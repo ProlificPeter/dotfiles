05e6dfdc1912b9ef4f15df0a124d231db4359897
ProlificPeter <peter@rldimensions.com>
2024-11-06 16:20:09 -0600

    emacs config

--- emacs/.emacs.d/personal/custom.el
+++ emacs/.emacs.d/personal/custom.el
@@ -3,17 +3,28 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
- ;; Use `nord8` from Nord's "Frost" palette as background color.
- (setq nord-region-highlight "frost")
- '(custom-enabled-themes '(nord-theme))
+ '(custom-enabled-themes '(nord))
  '(custom-safe-themes
-   '("38384bff3bbcce83e68f44b1a5af2c12c45030735c55eb02429f6d16eae672d6" default))
- '(xterm-mouse-mode t))
+   '("98b4ef49c451350c28a8c20c35c4d2def5d0b8e5abbc962da498c423598a1cdd" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "undefined" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default")))))
+ )
+
+;; Use `nord4` from Nord's "Snow Storm" palette as background color.
+(setq nord-region-highlight "snowstorm")
+
+
+
+;; (defun load-nord-theme (_frame)
+;;   (enable-theme 'nord))
+
+;; (add-hook 'after-make-frame-functions #'load-nord-theme)
+
+;; ----- ORG-MODE Customizations -----
+;; Enable org-mode TODOs to overwrite headline.
+(setq org-fontify-todo-headline t)
+
 (setq prelude-guru nil)
